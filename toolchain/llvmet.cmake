5ac6715ea2eb88af943302f180419279bf2d7ba5
razortail-qzxvk <a979747751@outlook.com>
2026-04-25 06:15:32 +0800

    0.0.0.7 elf

--- toolchain/llvmet.cmake
+++ toolchain/llvmet.cmake
@@ -3,6 +3,10 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 # 跳过链接测试
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+# 链接为elf后缀
+set(CMAKE_EXECUTABLE_SUFFIX_ASM ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_C   ".elf")
+set(CMAKE_EXECUTABLE_SUFFIX_CXX ".elf")
 
 # set(toolchain_path "C:/ATfE-22.1.0-Windows-x86_64")
 # set(CMAKE_ASM_COMPILER "${toolchain_path}/bin/clang.exe")
